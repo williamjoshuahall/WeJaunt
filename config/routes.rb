5f5d564fbfefd028002899c6e7a843d17c9551dd
Will Hall <williamjoshuahall@hotmail.com>
2015-04-15 12:51:32 +0100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 	get 'faq' => 'static_pages#faq'
 
 	get 'provider' => 'static_pages#provider'
+	
+	resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
